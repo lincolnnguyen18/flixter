a463790d15c3cf8d6c1ddc052027c7d65e1383d2
Lincoln Nguyen <letsmakeaworld@icloud.com>
2021-02-06 10:57:12 -0600

    Test fetch trailer links in trailer view

--- flixter/MovieTrailerViewController.swift
+++ flixter/MovieTrailerViewController.swift
@@ -8,13 +8,29 @@
 import UIKit
 
 class MovieTrailerViewController: UIViewController {
-    
+    var trailers = [[String:Any]]()
     var movieId: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print(movieId)
+        // print(movi!eId)
+        let url = URL(string: "https://api.themoviedb.org/3/movie/" + String(movieId) + "/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                // TODO: Get trailers
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                // TODO: Store the trailers in a property to use elsewhere
+                self.trailers = dataDictionary["results"] as! [[String:Any]]
+                print(self.trailers)
+            }
+        }
+        task.resume()
     }
     
 
