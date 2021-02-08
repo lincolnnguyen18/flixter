d0852657249f9e0d01fa6d80ada7c0719a1b2476
Lincoln Nguyen <letsmakeaworld@icloud.com>
2021-02-08 00:55:49 -0600

    View constraints fixed for home button less iPhones

---
+++ DerivedData/flixter/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/AlamofireImage.build/Objects-normal/x86_64/AlamofireImage_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/lincolnnguyen/Desktop/flixter/DerivedData/flixter/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/AlamofireImage.build/DerivedSources/AlamofireImage_vers.c
---
+++ DerivedData/flixter/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-flixter.build/Objects-normal/x86_64/Pods-flixter-dummy.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/lincolnnguyen/Desktop/flixter/Pods/Target\ Support\ Files/Pods-flixter/Pods-flixter-dummy.m \
+  /Users/lincolnnguyen/Desktop/flixter/DerivedData/flixter/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-flixter.build/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.4.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.4.sdk/usr/include/mach-o/dyld.modulemap
